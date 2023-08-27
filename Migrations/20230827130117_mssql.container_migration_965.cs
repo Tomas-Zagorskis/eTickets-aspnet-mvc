d91c476b505924600681171375f0e3501a364416
Tomas Zagorskis <tomzagos@gmail.com>
2023-08-27 17:43:25 +0300

    feat: add database

---
+++ Migrations/20230827130117_mssql.container_migration_965.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eTickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class mssqlcontainer_migration_965 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
