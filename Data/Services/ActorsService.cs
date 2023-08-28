ef19bffb8aafdce558cbb5a345c3761321f956cd
Tomas Zagorskis <tomzagos@gmail.com>
2023-08-28 21:29:05 +0300

    feat: add actors detail page and validation

--- Data/Services/ActorsService.cs
+++ Data/Services/ActorsService.cs
@@ -8,21 +8,23 @@ namespace eTickets.Data.Services {
         {
             _context = context;
         }
-        public void Add(Actor actor) {
-            throw new NotImplementedException();
+        public async Task AddAsync(Actor actor) {
+            await _context.Actors.AddAsync(actor);
+            await _context.SaveChangesAsync();
         }
 
         public void Delete(int id) {
             throw new NotImplementedException();
         }
 
-        public async Task<IEnumerable<Actor>> GetAll() {
+        public async Task<IEnumerable<Actor>> GetAllAsync() {
             var result = await _context.Actors.ToListAsync();
             return result;
         }
 
-        public Actor GetById(int id) {
-            throw new NotImplementedException();
+        public async Task<Actor> GetByIdAsync(int id) {
+            var result = await _context.Actors.FirstOrDefaultAsync(x => x.Id == id);
+            return result;
         }
 
         public Actor Update(int id, Actor newActor) {
