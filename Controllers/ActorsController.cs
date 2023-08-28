8ed5aea0bbe878519a919bbb61f82cc8df401691
Tomas Zagorskis <tomzagos@gmail.com>
2023-08-28 20:27:24 +0300

    feat: add actors create page

--- Controllers/ActorsController.cs
+++ Controllers/ActorsController.cs
@@ -13,5 +13,10 @@ namespace eTickets.Controllers {
             var data = await _service.GetAll();
             return View(data);
         }
+        
+        //Get: Actors/Create
+        public IActionResult Create() {
+            return View();
+        }
     }
 }
