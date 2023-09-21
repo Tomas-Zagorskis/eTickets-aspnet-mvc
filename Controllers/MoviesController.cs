f5ba31dce5ae0ea89c736e70cf57fb06a1180912
Tomas Zagorskis <tomzagos@gmail.com>
2023-09-21 23:08:11 +0300

    feat: edit movie and search

--- Controllers/MoviesController.cs
+++ Controllers/MoviesController.cs
@@ -16,6 +16,16 @@ namespace eTickets.Controllers {
             return View(allMovies);
         }
 
+        public async Task<IActionResult> Filter(string searchString) {
+            var allMovies = await _service.GetAllAsync(n => n.Cinema);
+
+            if (!string.IsNullOrEmpty(searchString)) {
+                var filteredResult = allMovies.Where(n => n.Name.ToLower().Contains(searchString.ToLower()) || n.Description.ToLower().Contains(searchString.ToLower())).ToList();
+                return View("Index", filteredResult);
+            }
+            return View("Index", allMovies);
+        }
+
         //GET: Movies/Details/1
         public async Task<IActionResult> Details(int id) {
             var movieDetail = await _service.GetMovieByIdAsync(id);
@@ -49,5 +59,52 @@ namespace eTickets.Controllers {
             return RedirectToAction(nameof(Index));
         }
 
-    }
+		//GET: Movies/Edit/1
+		public async Task<IActionResult> Edit(int id) {
+            var movieDetails = await _service.GetMovieByIdAsync(id);
+            if (movieDetails == null) return View("NotFound");
+
+            var response = new NewMovieVM()
+            {
+                Id = movieDetails.Id,
+                Name = movieDetails.Name,
+                Description = movieDetails.Description,
+                Price = movieDetails.Price,
+                ImageURL = movieDetails.ImageURL,
+                MovieCategory = movieDetails.MovieCategory,
+                CinemaId = movieDetails.CinemaId,
+                ProducerId = movieDetails.ProducerId,
+                EndDate = movieDetails.EndDate,
+                StartDate = movieDetails.StartDate,
+                ActorIds = movieDetails.Actors_Movies.Select(n => n.ActorId).ToList(),
+            };
+
+			var movieDropdowns = await _service.GetNewMovieDropdownsValues();
+
+			ViewBag.Cinemas = new SelectList(movieDropdowns.Cinemas, "Id", "Name");
+			ViewBag.Producers = new SelectList(movieDropdowns.Producers, "Id", "FullName");
+			ViewBag.Actors = new SelectList(movieDropdowns.Actors, "Id", "FullName");
+
+			return View(response);
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> Edit(int id, NewMovieVM movie) {
+            if (id != movie.Id) return View("NotFound");
+
+			if (!ModelState.IsValid) {
+				var movieDropdowns = await _service.GetNewMovieDropdownsValues();
+
+				ViewBag.Cinemas = new SelectList(movieDropdowns.Cinemas, "Id", "Name");
+				ViewBag.Producers = new SelectList(movieDropdowns.Producers, "Id", "FullName");
+				ViewBag.Actors = new SelectList(movieDropdowns.Actors, "Id", "FullName");
+
+				return View(movie);
+			}
+
+			await _service.UpdateMovieAsync(movie);
+			return RedirectToAction(nameof(Index));
+		}
+
+	}
 }
