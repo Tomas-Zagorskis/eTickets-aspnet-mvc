881e7b92d023503144e289901e5de1c017fdd608
Tomas Zagorskis <tomzagos@gmail.com>
2023-09-22 10:15:32 +0300

    feat: add shopping cart models and migrations

---
+++ Models/OrderItem.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace eTickets.Models {
+    public class OrderItem {
+        [Key]
+        public int Id { get; set; }
+
+        public int Amount { get; set; }
+        public double Price { get; set; }
+
+        public int MovieId { get; set; } // MovieId as foreign key for Movie
+        public virtual Movie Movie { get; set; }
+    }
+}
